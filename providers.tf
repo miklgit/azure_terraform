475c7d6c3433a4664cbd99f51f0da82f4eb3f88e
mikl_solo <miklardv@gmail.com>
2025-09-25 09:20:44 +0200

    :construction_worker: Init CI

--- providers.tf
+++ providers.tf
@@ -23,11 +23,14 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.azure_subscription_id
+  # subscription_id = var.azure_subscription_id
   # tenant_id       = var.azure_tenant_id
+  # client_id       = var.azure_client_id
+  # client_secret   = var.azure_client_secret
+  subscription_id = "cf5274c6-d6b9-4ab3-a8d7-afd83781e6b6"
   tenant_id       = "297246f1-5c28-4f6f-88aa-8990cb7383b1"
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
+  client_id       = "14e0a936-0371-45fc-b3ff-32c154a9a121"
+  client_secret   = "Eob8Q~C-g5lTeCDFQ9ZibR3waJtIbdGvurhzxbCA"
 }
 
 provider "azapi" {
